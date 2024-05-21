cd94eabae2976fcafbb7eaceb1c0a0be27a5d928
alex <alex@nanoinsure.com>
2024-05-21 09:18:06 +0800

    - 功能：创建实体 entity - 逻辑  1.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import { Post } from './entity/Post';
 
 createConnection().then(async connection => {
-    console.log(connection);
+    const posts=await connection.manager.find(Post)
+    console.log(`posts`, posts) ;
     connection.close()
 }).catch(error => console.log(error));
