f592ae5cbd2e11cce7b9eed70945e3cbd5cf4dbf
alex <alex@nanoinsure.com>
2024-07-13 11:32:55 +0800

    - 功能: add deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,8 @@
+docker start 05a7f7609990 &&
+cd /home/blog/app &&
+git pull &&
+npm install --production=false &&
+npm run build &&
+docker build -t alex/node-web-app . &&
+docker run --name app --network=host -p 3000:3000 -d alex/node-web-app &&
+echo 'OK'
