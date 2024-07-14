82b06053e55d38ec8aa9f8b078650b4886d32e87
alex <alex@nanoinsure.com>
2024-07-14 10:09:26 +0800

    - 功能: update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,9 @@ docker start 05a7f7609990 &&
 cd /home/blog/app &&
 git pull &&
 npm install --production=false &&
+git apply migration.patch &&
+npm run m:run &&
+git reset --hard HEAD &&
 npm run build &&
 docker build -t alex/node-web-app . &&
 docker rm -f app &&
