3360f2c35ebe43c9dd8756c8a4d7fc470180b706
alex <alex@nanoinsure.com>
2024-07-16 22:28:17 +0800

    - 功能: 添加antd组件

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "styles/output.css";
 import type { AppProps } from "next/app";
 import { useCallback } from "react";
 import { useRouter } from "next/router";
+import { Form, Input, Modal } from "antd";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -11,13 +12,44 @@ export default function App({ Component, pageProps }: AppProps) {
     },
     [router]
   );
+  const openModal = useCallback((modalType) => {
+    Modal.info({
+      title: "登录",
+      content: (
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Form>
+      ),
+    });
+  }, []);
   return (
     <>
       <div className={"tw-flex tw-justify-between"}>
         <p>博客</p>
         <div>
-          <button onClick={() => navigate("/sign_in")}>登录|</button>
-          <button onClick={() => navigate("/sign_up")}> 注册</button>
+          <button onClick={() => openModal("sign_in")}>登录|</button>
+          <button onClick={() => openModal("sign_up")}> 注册</button>
         </div>
       </div>
       <Component {...pageProps} />;
