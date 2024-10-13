feadc78c893a8c27722975efe74ca0a00d91ce86
alex <alex@nanoinsure.com>
2024-10-13 18:24:12 +0800

    初始化用户详情页面的文章展示

--- components/user/article.list.tsx
+++ components/user/article.list.tsx
@@ -1,16 +1,43 @@
 import { KeyValMap } from "@/constants/common-type";
+import { formatDate } from "@/utils/date.utils";
+import { CommentOutlined, LikeOutlined } from "@ant-design/icons";
 import { NextPage } from "next";
-
+import { useRouter } from "next/router";
+import styles from "./styles/articleList.module.scss";
 type Props = {
   posts: KeyValMap;
 };
 const ArticleList: NextPage<Props> = (props) => {
   const { posts: postsData } = props;
-  console.log("postsData", postsData);
+  const router = useRouter();
+
+  const navToPostDetail = (uuid: string) => {
+    router.push(`/posts/${uuid}`);
+  };
   return (
     <div>
       {postsData.map((post: KeyValMap) => (
-        <div key={post.id}>{post.content}</div>
+        <div
+          key={post.id}
+          className={`${styles.postCard} tw-flex tw-flex-col tw-cursor-pointer`}
+          onClick={() => navToPostDetail(post.uuid)}
+        >
+          <div className={styles.mainInfo}>
+            <div className={`${styles.postTitle}`}>{post.title}</div>
+            <div className={`${styles.postContent}`}>{post.content}</div>
+          </div>
+          <div className="subInfo tw-flex tw-items-center tw-gap-4">
+            <div className="createdAt">{formatDate(post.createdAt)}</div>
+            <p className="likeCount tw-m-0">
+              <LikeOutlined />
+              {post.likesAmt}
+            </p>
+            <p className="commentCount tw-m-0">
+              <CommentOutlined />
+              {post?.comments?.length}
+            </p>
+          </div>
+        </div>
       ))}
     </div>
   );
--- utils/date.utils.ts
+++ utils/date.utils.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 
 export function formatDate(
   isoDate: string | Date,
-  format: string = "YYYY-MM-DD"
+  format: string = "YYYY/MM/DD"
 ) {
   if (isoDate instanceof Date) {
     isoDate = isoDate.toISOString();
